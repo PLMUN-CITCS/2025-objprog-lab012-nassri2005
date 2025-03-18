7fcb3a9990afa7d9d770bd4783f12cbbe5a42879
Nassri Lakiman <lakimannassri@bscs@plmun.edu.ph>
2025-03-18 19:53:08 +0800

    nass12

---
+++ MethodDemo.java
@@ -0,0 +1,17 @@
+public class MethodDemo {
+   
+    public static void greet() {
+        System.out.println("Hello! Welcome to the program.");
+    }
+
+   
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+
+ 
+    public static void main(String[] args) {
+        greet(); 
+        printMessage("Hello, this is a message!"); 
+    }
+}
